bda545b806dc9d6f929ef4052073751ee893894f
ParteyBerlin <66954138+ParteyBerlin@users.noreply.github.com>
2021-01-17 13:38:01 +0100

    IBAN-calculator
    ...added comments

--- Iban/src/Iban.java
+++ Iban/src/Iban.java
@@ -3,39 +3,46 @@ import java.math.*;
 
 public class Iban {
 
+	/**
+	 * generates an IBAN by entering account-number and bank-code
+	 */
 	public static void main(String[] args) 
 	{
-		//Eingabe
+		//enter account number
 		Scanner scn = new Scanner(System.in);
 		System.out.println("Konto eingeben: ");
 		String ktoEingabe = scn.next();
 		
 		
-			//Kurze Kontonummern mit Nullen auffüllen
+			//if account number has less than 10 digits, it will be extended to 10 digits by putting 0 at the beginning
 				while(ktoEingabe.length() < 10)
 				{
 					ktoEingabe = "0" + ktoEingabe;
 				}
 			
-			
+		//enter bank-code
 		System.out.println("BLZ eingeben: ");
 		String blzEingabe = scn.next();
+		
+		//build a String by connecting bank code, account number and a check number for germany (131400)		
 		String zeichenkette = blzEingabe+ktoEingabe+131400;
 		
+		
 		//////////////////////////////////////////////////////
-		//Prüfziffer berechnen und zurückgeben//
+		//Prüfziffer berechnen und zurückgeben//calculate IBAN-checknumber//
 		int pruefziffer = generateIbanChecksum(zeichenkette);
 		
 		
 		//////////////////////////////////////////////////////
-		//IBAN generieren und ausgeben//
+		//IBAN generieren und ausgeben//calculate IBAN//
 		generateIban(pruefziffer, blzEingabe, ktoEingabe);
 	}
 	
 	
 	public static int generateIbanChecksum(String kompletteKette)
 	{
-		//BigInteger anlegen um Modulo zu berechnen und Werter zuweisen
+		//BigInteger anlegen um Modulo zu berechnen und Werte zuweisen
+		//use BigInteger for the given String
 		BigInteger zeichenkette;
 		BigInteger modulo;
 		BigInteger restBig;
@@ -44,16 +51,19 @@ public class Iban {
 		
 		
 		//Modulo berechnen
+		//calculate modulo 97 of the given String and save it in restBig
 		restBig = zeichenkette.mod(modulo);
 				
 		
 		//Integer anlegen und BigInteger in int umwandeln um Minus rechnen zu können
+		//convert from BigInteger to Integer to be able to calculate the next step
 		Integer restInt;
 		restInt = restBig.intValue();
 		
 		
 		//Minus rechnen
 		int pruefzifferInt = 98 - restInt;
+		//returns the IBAN-checknumber
 		return pruefzifferInt;
 	}
 	
